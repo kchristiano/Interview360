3d4e201aa8d651c2e1787699a6848ec6714dc472
Kevin Christiano <kchristiano@life360.com>
2024-02-07 17:26:42 -0500

    Adding framework

---
+++ Aficianado.xcframework/ios-arm64_x86_64-simulator/Aficianado.framework/Headers/Aficianado.h
@@ -0,0 +1,18 @@
+//
+//  Aficianado.h
+//  Aficianado
+//
+//  Created by Kevin Christiano on 1/19/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Aficianado.
+FOUNDATION_EXPORT double AficianadoVersionNumber;
+
+//! Project version string for Aficianado.
+FOUNDATION_EXPORT const unsigned char AficianadoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aficianado/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Interview360",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "Interview360",
+            targets: ["Aficianado"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Aficianado",
+            path: "Aficianado.xcframework")
+    ]
+)
