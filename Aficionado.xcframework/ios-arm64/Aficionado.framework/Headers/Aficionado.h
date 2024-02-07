dbc292bf54980c3b901f239c26bd3d033e7ff03d
Kevin Christiano <kchristiano@life360.com>
2024-02-07 18:44:04 -0500

    Replacing framework

---
+++ Aficionado.xcframework/ios-arm64/Aficionado.framework/Headers/Aficionado.h
@@ -0,0 +1,18 @@
+//
+//  Aficionado.h
+//  Aficionado
+//
+//  Created by Kevin Christiano on 2/7/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Aficionado.
+FOUNDATION_EXPORT double AficionadoVersionNumber;
+
+//! Project version string for Aficionado.
+FOUNDATION_EXPORT const unsigned char AficionadoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aficionado/PublicHeader.h>
+
+
